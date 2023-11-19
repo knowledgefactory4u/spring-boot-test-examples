2f813d55961280590536bab061600ce98d9dae4e
Sibin Muhammed <54763130+knowledgefactory4u@users.noreply.github.com>
2023-11-20 00:53:16 +0530

    Update DataJpaTestExampleApplication.java

--- datajpatest-example/src/main/java/com/knf/dev/demo/DataJpaTestExampleApplication.java
+++ datajpatest-example/src/main/java/com/knf/dev/demo/DataJpaTestExampleApplication.java
@@ -1,15 +1,10 @@
 package com.knf.dev.demo;
 
-import com.knf.dev.demo.repository.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DataJpaTestExampleApplication {
-
-	@Autowired
-	StudentRepository studentRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataJpaTestExampleApplication.class, args);
