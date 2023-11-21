858a981e8d0ef4331f2a1ed44713981c877e5587
Sibin Muhammed <54763130+knowledgefactory4u@users.noreply.github.com>
2023-11-22 00:28:58 +0530

    Add files via upload

---
+++ datamongotest-example/src/main/java/com/knf/dev/demo/repository/StudentRepository.java
@@ -0,0 +1,19 @@
+package com.knf.dev.demo.repository;
+
+import com.knf.dev.demo.document.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentRepository extends MongoRepository<Student, String> {
+
+    @Query("{name : ?0}")
+    Optional<Student> findByName(String name);
+
+    @Query("{ age : { $gte: ?0 } }")
+    List<Student> findByAgeGreaterThan(Integer age);
+
+    List<Student> findByAgeLessThan(Integer age);
+}
---
+++ datamongotest-example/src/test/java/com/knf/dev/demo/StudentRepositoryTest.java
@@ -0,0 +1,81 @@
+package com.knf.dev.demo;
+
+import com.knf.dev.demo.document.Student;
+import com.knf.dev.demo.repository.StudentRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@DataMongoTest
+public class StudentRepositoryTest {
+
+   // @Autowired
+   //private MongoTemplate mongoTemplate;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    //Method should be executed before all tests in the current test class
+    //Load initial data
+    @BeforeAll
+    static void setup(@Autowired StudentRepository studentRepository) {
+
+        Student student1 = new Student("101","Alpha","alpha@knf.com",50);
+        Student student2 = new Student("102","Beta","beta@knf.com",40);
+        Student student3 = new Student("103","Gama","gama@knf.com",30);
+        Student student4 = new Student("104","Pekka","pekka@knf.com",20);
+
+        List<Student> students = Arrays.asList(student1,student2,student3,student4);
+
+        studentRepository.saveAll(students);
+
+    }
+
+    @Test
+    void findByName_ReturnsTheStudent() {
+
+        Student student = studentRepository.findByName("Alpha").get();
+        assertThat(student).isNotNull();
+        assertThat(student.getEmail()).isEqualTo("alpha@knf.com");
+        assertThat(student.getName()).isEqualTo("Alpha");
+        assertThat(student.getId()).isEqualTo("101");
+        assertThat(student.getAge()).isEqualTo(50);
+    }
+
+    @Test
+    void findByAgeGreaterThan_ReturnsTheListStudents() {
+
+        List<Student> students = studentRepository.findByAgeGreaterThan(29);
+
+        //Convert list of students to list of id(String)
+        List<String> ids = students.stream()
+                .map(o -> o.getId())
+                .collect(Collectors.toList());
+
+
+        assertThat(students.size()).isEqualTo(3);
+        assertThat(ids).hasSameElementsAs(Arrays.asList("103","102","101"));
+    }
+
+    @Test
+    void findByAgeLessThan_ReturnsTheListStudents() {
+
+        List<Student> students = studentRepository.findByAgeLessThan(31);
+
+        //Convert list of students to list of id(Integer)
+        List<String> ids = students.stream()
+                .map(o -> o.getId())
+                .collect(Collectors.toList());
+
+        assertThat(students.size()).isEqualTo(2);
+        assertThat(ids).hasSameElementsAs(Arrays.asList("104","103"));
+    }
+}
